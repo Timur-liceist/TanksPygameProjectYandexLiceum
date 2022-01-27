5a781aeea71850b7a6ad00ee012b721e1087a38f
Тимур Асланджонович Очилов <79978867+Timur-liceist@users.noreply.github.com>
2022-01-27 09:52:34 +0300

    Delete pythonProject2 directory

--- pythonProject2/d.py
+++
@@ -1,8 +0,0 @@
-l = int(input())
-# r = map(int, input().split())
-r = int(input())
-t = 0
-for i in range(l, r + 1):
-    if list(map(int, list(str(i)))) == sorted(list(map(int, list(str(i))))):
-        t += 1
-print(t)
